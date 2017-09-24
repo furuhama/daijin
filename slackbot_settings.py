64597a8a2fcc6398645028de75d0676b903c98ef
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-09-24 23:53:32 +0900

    small fix

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,11 +1,11 @@
-
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 # In[ ]:
 
 default_reply = "Hello world!"
 
 import os
+# heroku の環境変数でAPI_TOKENを取得
 API_TOKEN = os.getenv("API_TOKEN")
 
 PLUGINS = [
